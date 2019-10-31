0679b55f063d08405a09c0e4a81c7d6e842bce9b
Roberto Lombilla <ironmanbomb@hotmail.com>
2019-10-31 21:47:31 +0000

    Pics

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,12 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def show 
+    @photo = Photo.new
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_142055) do
+ActiveRecord::Schema.define(version: 2019_10_31_150536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+  end
 
   create_table "places", force: :cascade do |t|
     t.string "name"
